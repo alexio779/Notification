17558b64e9d2733ae4287df6b21b8adfffb7088f
Alex <alex.bul777@yandex.ru>
2023-04-03 00:24:36 +0300

    grouping

--- app/src/main/java/com/example/notifications/MainActivity.java
+++ app/src/main/java/com/example/notifications/MainActivity.java
@@ -18,64 +18,101 @@ import androidx.core.app.NotificationManagerCompat;
 
 
 public class MainActivity extends AppCompatActivity {
-
     public final static String GROUP_KEY = "GK";
     public final static String CHANNEL_ID = "channel_id";
-
+    public final static int FIRST_NOTIFY_ID = 1;
+    public final static int SECOND_NOTIFY_ID = 2;
+    public final static int SUMMARY_NOTIFY_ID = 3;
     NotificationManagerCompat notificationManagerCompat;
-    Notification notification;
 
-    @RequiresApi(api = Build.VERSION_CODES.S)
+    Notification notification, notification2, notification3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        PendingIntent InterestingIntent=PendingIntent.getActivity(MainActivity.this,0,
-                new Intent(MainActivity.this,MainActivity.class),PendingIntent.FLAG_MUTABLE);
-
-        String text = "Текст уведомления";
-        String title = "Заголовок уведомления";
-        String btn_title = "Не интересно";
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "myCh")
-                .setSmallIcon(android.R.drawable.stat_notify_chat)
-                .addAction(0, btn_title, InterestingIntent)
-                .setContentTitle(title)
-                .setGroupSummary(true)
-                .setGroup(GROUP_KEY)
-                .setContentInfo("123")
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setContentText(text);
-
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-        {
-            NotificationChannel channel = new NotificationChannel("myCh", "my Channel",
-                    NotificationManager.IMPORTANCE_DEFAULT);
-
-            NotificationManager manager = getSystemService(NotificationManager.class);
-            manager.createNotificationChannel(channel);
-
-        }
-
-        notification = builder.build();
-        notificationManagerCompat = NotificationManagerCompat.from(this);
 
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.S)
             @Override
             public void onClick(View v)
             {
-                notificationManagerCompat.notify(1, notification);
-                notificationManagerCompat.notify(2, notification);
+                firstNotification();
+                secondNotification();
+                summaryNotification();
             }
-
-            public void smt(){
-
-            }
-
         });
     }
 
+//--------------------------------------------------------------------------------------------------
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    private void firstNotification()
+    {
+        PendingIntent InterestingIntent=PendingIntent.getActivity(MainActivity.this,0,
+                new Intent(MainActivity.this,MainActivity.class),PendingIntent.FLAG_MUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.stat_notify_chat)
+                .addAction(0, "Не интересно", InterestingIntent)
+                .setContentTitle("2")
+                .setGroup(GROUP_KEY)
+                .setContentInfo("Первое уведомление")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentText("text");
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "my Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+        notification = builder.build();
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+        setContentView(R.layout.activity_main);
+
+        notificationManagerCompat.notify(FIRST_NOTIFY_ID, notification);
+    }
+
+//--------------------------------------------------------------------------------------------------
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    private void secondNotification()
+    {
+        PendingIntent InterestingIntent=PendingIntent.getActivity(MainActivity.this,0,
+                new Intent(MainActivity.this,MainActivity.class),PendingIntent.FLAG_MUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.stat_notify_chat)
+                .addAction(0, "Не интересно", InterestingIntent)
+                .setContentTitle("1")
+                .setGroup(GROUP_KEY)
+                .setContentInfo("Второе уведомление")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentText("text");
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "my Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+        notification2 = builder.build();
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+        setContentView(R.layout.activity_main);
+
+        notificationManagerCompat.notify(SECOND_NOTIFY_ID, notification2);
+    }
+//--------------------------------------------------------------------------------------------------
+    private void summaryNotification()
+    {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.stat_notify_chat)
+                .setGroup(GROUP_KEY)
+                .setGroupSummary(true);
+        notification3 = builder.build();
+
+        notificationManagerCompat.notify(SUMMARY_NOTIFY_ID, notification3);
+    }
 }
